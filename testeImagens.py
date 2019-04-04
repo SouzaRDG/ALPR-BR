e266f791193779cd8d35c86d5ad98580a0bdb2f8
SouzaRDG <souzardg@yahoo.com>
2019-04-04 20:20:46 -0300

    xxx

--- testeImagens.py
+++ testeImagens.py
@@ -1,6 +1,6 @@
 
 
-###### TENHAM CERTEZA QUE ESTÃO USANDO PYTHON 3 OU MAIOR
+###### TENHAM CERTEZA QUE ESTÃO USANDO PYTHON 3
 
 
 
@@ -13,7 +13,7 @@ import numpy as np
 from PIL import Image
 
 
-###   AQUI VAMOS POR OS METODOS QUE SÃO MAIORES QUE UMA LINHA!
+###   AQUI VAMOS POR OS METODOS QULSTME SÃO MAIORES QUE UMA LINHA!
 ###   ASSIM FICA MAIS LIMPO E FACIL DE IDENTIFICAR O QUE ESTÁ SENDO VIZUALIZADO!!!
 
 
@@ -34,18 +34,21 @@ def altoCont (imagem):
 
 
 # image = cv2.imread('placas/cam1.png')
-image = cv2.imread('placas/images/01.jpg')
+image = cv2.imread('placas/images/00.jpg')
 
 
 imgGray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
 imgAltoCon = altoCont(imgGray)
-imgBlur = cv2.blur(imgAltoCon, (7, 7) )
-# _ ,imgThresh = cv2.threshold(imgAltoCon, 135 ,255,cv2.THRESH_BINARY_INV,cv2.THRESH_OTSU )
-_ ,imgThresh = cv2.threshold(imgBlur, 110 ,255,cv2.THRESH_BINARY_INV,cv2.THRESH_OTSU )
-_ ,imgThresh2 = cv2.threshold(imgThresh, 180 ,255,cv2.THRESH_BINARY, cv2.ADAPTIVE_THRESH_GAUSSIAN_C)
-kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, ( 3 , 3 ))
+imgBlur = cv2.blur(imgAltoCon, (6, 6) )
+_ ,imgThresh = cv2.threshold(imgBlur, 110 ,255, cv2.ADAPTIVE_THRESH_MEAN_C , cv2.THRESH_BINARY_INV)
+# _ ,imgThresh = cv2.threshold(imgBlur, 100 ,255, cv2.THRESH_OTSU , cv2.THRESH_BINARY_INV)
+# _ ,imgThresh = cv2.threshold(imgBlur, 110 ,255,cv2.THRESH_BINARY_INV,cv2.THRESH_OTSU )
+# _ ,imgThresh2 = cv2.threshold(imgThresh, 120 ,255,cv2.THRESH_BINARY_INV, cv2.THRESH_OTSU)
+kernel = cv2.getStructuringElement(cv2.MORPH_DILATE, ( 3 , 3 ))
 imgDilated = cv2.dilate(imgThresh,kernel, iterations = 3)
-contours, _ = cv2.findContours(imgDilated,cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+contours, _ = cv2.findContours(imgDilated,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+# cv2.imshow("sss", image)
 
 
 listaPlacas = list()
@@ -61,9 +64,9 @@ for contour in contours:
     if w > 3.1 * h:
         continue
 
-    cv2.rectangle(imgDilated,(x,y),(x+w,y+h),(150,0,155),3)
+    cv2.rectangle(imgDilated, (x,y), (x+w,y+h) , 100 , 3)
 
-    imgPossivelPlaca = imgDilated[y:y+h,x:x+w]
+    imgPossivelPlaca = image[y+35:y+h-18 , x+20:x+w-20]
 
     imgResize = cv2.resize(imgPossivelPlaca, (0, 0), fx=3, fy=3)
 
@@ -71,37 +74,55 @@ for contour in contours:
 
     listaPlacas.append(novaPlaca)
 
+
 for placa in listaPlacas:
 
-    # cv2.imshow(placa.nome,placa.image)
 
-    # make small details dissapear
-    result = cv2.dilate(placa.image, np.ones((5, 5), np.uint8), iterations=1)
+    placa.image = altoCont(placa.image)
+    placa.image = cv2.cvtColor(placa.image, cv2.COLOR_RGB2GRAY)
+    placa.image = altoCont(placa.image)
+
+    _ , placa.image = cv2.threshold(placa.image, 150 ,255, cv2.THRESH_OTSU , cv2.THRESH_BINARY_INV)
+    cv2.imshow("x", placa.image)
+
     # those which weren't that small are back but there are less of them
-    result = cv2.erode(result, np.ones((5, 5), np.uint8), iterations=2)
-
-    result = cv2.Canny(result, 80, 255, apertureSize=3, L2gradient=True)
-
-
+    placa.image = cv2.erode(placa.image, np.ones((5, 5), np.uint8), iterations=2)
+    # make small details disapear
+    placa.image = cv2.dilate(placa.image, np.ones((5, 5), np.uint8), iterations=6)
 
 
-
+    placa.image = cv2.Canny(placa.image , 100, 255, apertureSize = 3, L2gradient=True)
 
 
 
-
-    #
-    # text = ocr.image_to_string(Image.fromarray(result), config="-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    # text = ocr.image_to_string(Image.fromarray(placa.image), "mittSemHifen" ,config="-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     # text = ' '.join(text.split())
 
+    # print(text + ' ' + placa.nome)
 
-    cv2.imshow(placa.nome,result)
+
+    cv2.imshow(placa.nome,placa.image)
+
+    text = ocr.image_to_string(placa.image, "charplaca")
+    print(text + ' ' + placa.nome)
+
+    text = ocr.image_to_string(placa.image, "mittSemHifen")
+    print(text + ' ' + placa.nome)
+
+    text = ocr.image_to_string(placa.image, "mittComHifen")
+    print(text + ' ' + placa.nome)
+
+    text = ocr.image_to_string(placa.image, "manSemHifen")
+    print(text + ' ' + placa.nome)
+
+    text = ocr.image_to_string(placa.image, "manComHifen")
+    print(text + ' ' + placa.nome)
+
+    text = ocr.image_to_string(placa.image)
+    print(text + ' ' + placa.nome)
 
     # cv2.imwrite(placa.nome + '.png', placa.image)
 
-    text = ocr.image_to_string(result)
-
-    print(text + ' ' + placa.nome)
 
 
 
@@ -109,9 +130,7 @@ for placa in listaPlacas:
 
 
 # cv2.imshow("image", image)
-# # #cv2.imshow("gray",gray)
-# # #cv2.imshow("thresh", thresh)
-cv2.imshow("dilated", imgDilated)
+# cv2.imshow("dilated", imgDilated)
 
 cv2.waitKey()
 # cv2.destroyAllWindows()
